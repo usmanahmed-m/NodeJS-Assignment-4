264544dccb5291098fb664d415ecde5d660b7882
Usman Ahmed <69243669+usmanahmed-m@users.noreply.github.com>
2023-07-29 13:32:53 +0500

    added cluster module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// const cluster = require('cluster');
+const cluster = require('cluster');
+const os = require('os');
 const express = require('express');
 const dotenv = require('dotenv');
 const dbConnection = require('./config/dbConn');
@@ -6,24 +7,18 @@ const routes = require('./routes/routes');
 
 const app = express();
 
-// const os = require('os');
-
-// // if (cluster.isMaster) {
-// /     cluster.fork();
-// //   }
-// // }/   for (let i = 0; i < os.cpus().length; i++) {
-// //     console.log(process.pid);
-// //
-
-// console.log(os.cpus().length);
-
 dotenv.config();
 
-routes(app);
+if (cluster.isMaster) {
+  for (let i = 0; i < os.cpus().length; i += 1) {
+    cluster.fork();
+  }
+} else {
+  routes(app);
 
-const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  dbConnection();
-  // console.log(`Listening on PORT ${PORT}`);
-});
+  app.listen(PORT, () => {
+    dbConnection();
+  });
+}
